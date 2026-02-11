90189b47b3b9d4105a4d749be97c8a6900df546d
Afonso Mendes de Carvalho <afonso@MacBook-Pro-de-Afonso.local>
2026-02-11 00:18:11 +0000

    Remove floating dots

--- src/components/FloatingElements.tsx
+++ src/components/FloatingElements.tsx
@@ -37,36 +37,6 @@ export function FloatingElements() {
         }}
       />
 
-      {/* Small floating dot */}
-      <motion.div
-        className="absolute w-4 h-4 rounded-full bg-gray-300"
-        style={{ top: "30%", left: "20%" }}
-        animate={{
-          y: [0, -20, 0],
-          opacity: [0.5, 1, 0.5],
-        }}
-        transition={{
-          duration: 4,
-          repeat: Infinity,
-          ease: "easeInOut",
-        }}
-      />
-
-      {/* Another small dot */}
-      <motion.div
-        className="absolute w-3 h-3 rounded-full bg-gray-200"
-        style={{ top: "60%", right: "25%" }}
-        animate={{
-          y: [0, 15, 0],
-          opacity: [0.3, 0.7, 0.3],
-        }}
-        transition={{
-          duration: 5,
-          repeat: Infinity,
-          ease: "easeInOut",
-          delay: 1,
-        }}
-      />
     </div>
   );
 }
